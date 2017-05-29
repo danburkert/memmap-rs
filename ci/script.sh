09702a3f54c0db1da6245d29dddd7776fbca1a1f
Dan Burkert <dan@danburkert.com>
2017-05-29 13:59:17 -0700

    travis: skip doctest with rustc 1.8.0

--- ci/script.sh
+++ ci/script.sh
@@ -9,10 +9,13 @@ main() {
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
+    elif [ ! -z $SKIP_DOCTEST ]; then
+      cross test --target $TARGET --lib
+      cross test --target $TARGET --release --lib
+    else
+      cross test --target $TARGET
+      cross test --target $TARGET --release
     fi
-
-    cross test --target $TARGET
-    cross test --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
